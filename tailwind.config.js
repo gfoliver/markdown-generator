fb6e90a488e4c805fd993e2a856fba730bf370b7
Guilherme Fleck Oliveira <guilhermefleckoliveira@gmail.com>
2023-03-15 13:06:55 -0300

    Editor frontend + markdown preview

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  darkMode: 'class',
+  theme: {
+    extend: {
+        colors: {
+            primary: '#5227FF',
+            dark: '#0A0A21',
+            'dark-lighter': 'rgba(255, 255, 255, 0.1)',
+            tint: 'rgba(0, 0, 0, 0.05)',
+            text: '#000000',
+            'text-dark': '#FFF',
+            light: '#F3F3F3',
+            lighter: '#FCFCFC'
+        }
+    },
+  },
+  plugins: [],
+}
